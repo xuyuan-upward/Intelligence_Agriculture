32649d8f8ca877afd35296ddbb5a123d70ce9e4f
YuanXu <2517115657@qq.com>
2026-01-14 10:44:15 +0800

    实时2s通过MQTT推送后端,WebSocket推送前端功能实现,在线状态存储初步实现

--- src/main/java/yuan/xu/intelligence_agriculture/IntelligenceAgricultureApplication.java
+++ src/main/java/yuan/xu/intelligence_agriculture/IntelligenceAgricultureApplication.java
@@ -1,9 +1,13 @@
 package yuan.xu.intelligence_agriculture;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@MapperScan("yuan.xu.intelligence_agriculture.mapper")
 public class IntelligenceAgricultureApplication {
 
     public static void main(String[] args) {
--- src/main/java/yuan/xu/intelligence_agriculture/exp/PreInvNotEnoughException.java
+++ src/main/java/yuan/xu/intelligence_agriculture/exp/PreInvNotEnoughException.java
@@ -1,26 +1,26 @@
-package yuan.xu.intelligence_agriculture.exp;
-
-import com.dsl.cdp.common.constant.ResultCodeEnum;
-import com.dsl.cdp.common.exception.BaseException;
-import org.springframework.http.HttpStatus;
-
-/**
- * 用于o2o前置判断库存不足，此异常不能在扣减库存逻辑时使用，不然会有问题
- * @description:
- * @author: caorunlong
- * @date: 2024/3/8
- */
-public class PreInvNotEnoughException extends BaseException {
-
-    public PreInvNotEnoughException() {
-        this(ResultCodeEnum.BAD_REQUEST.code());
-    }
-
-    public PreInvNotEnoughException(String code) {
-        super(code, HttpStatus.BAD_REQUEST, ResultCodeEnum.BAD_REQUEST.getDesc());
-    }
-
-    public PreInvNotEnoughException(String code, String reason) {
-        super(code, HttpStatus.BAD_REQUEST, reason);
-    }
-}
+//package yuan.xu.intelligence_agriculture.exp;
+//
+//import com.dsl.cdp.common.constant.ResultCodeEnum;
+//import com.dsl.cdp.common.exception.BaseException;
+//import org.springframework.http.HttpStatus;
+//
+///**
+// * 用于o2o前置判断库存不足，此异常不能在扣减库存逻辑时使用，不然会有问题
+// * @description:
+// * @author: caorunlong
+// * @date: 2024/3/8
+// */
+//public class PreInvNotEnoughException extends BaseException {
+//
+//    public PreInvNotEnoughException() {
+//        this(ResultCodeEnum.BAD_REQUEST.code());
+//    }
+//
+//    public PreInvNotEnoughException(String code) {
+//        super(code, HttpStatus.BAD_REQUEST, ResultCodeEnum.BAD_REQUEST.getDesc());
+//    }
+//
+//    public PreInvNotEnoughException(String code, String reason) {
+//        super(code, HttpStatus.BAD_REQUEST, reason);
+//    }
+//}
